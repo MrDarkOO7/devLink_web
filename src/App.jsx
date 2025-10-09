4b5de56591ff51efeda1f8c1a311c4b0c23f6200
MrDarkOO7 <m.waleedapsacian@gmail.com>
2025-10-09 12:12:57 +0500

    Add routing and new components for user authentication and profile management
    - Integrated react-router-dom for navigation
    - Created Login, Profile, Body, and Footer components
    - Updated App component to include routing logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import Navbar from "./components/Navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
