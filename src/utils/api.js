6e2f727f60c855623556ce16884a5283a924df87
MrDarkOO7 <m.waleedapsacian@gmail.com>
2025-10-17 16:15:06 +0500

    url update

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = "http://localhost:8080";
+export const API_BASE = "http://44.220.134.112/api";
 
 const api = axios.create({
   baseURL: API_BASE,
